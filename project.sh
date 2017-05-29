2de3e283eb1744932f3afb7dfbf887c31b2d16ee
Ayush Bahuguna <lespaulayush@gmail.com>
2017-05-29 08:37:26 +0530

    add test commands

--- project.sh
+++ project.sh
@@ -9,11 +9,31 @@ echo $core_dependencies
 echo $dev_dependencies
 yarn add $core_dependencies --save
 yarn add $dev_dependencies --dev
+echo "
+,{
+	"test": "node node_modules/.bin/mocha --reporter spec --require ./server/test/testHelper.js --timeout 10000 --compilers js:babel-core/register",
+	"start": "babel-node app.js --presets es2015,stage-2"
+}
+" >> package.json
+touch server/test/testHelper.js
+echo "require('babel-core/register')({
+  ignore: /node_modules\/(?!ProjectB)/
+});" >> server/test/testHelper.js
+touch .babelrc
+echo "{
+	"presets": ["es2015"]
+}" >> .babelrc
 mkdir client server
 touch app.js
-echo "Hello world" >> app.js
+echo "import express from 'express'
+ const app = express();
+ app.listen(process.env.PORT, process.env.IP, ()=>{
+ console.log(`Express server started`);
+})" >> app.js
 mkdir server/models server/routes server/test
 mkdir client/dist
 mkdir client/src
 mkdir client/src/app
-node app.js
+process.env.PORT=6655
+process.env.IP='localhost'
+npm start
